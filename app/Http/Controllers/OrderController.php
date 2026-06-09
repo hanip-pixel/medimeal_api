f8dd684cd6d0ecd2ea9e3d7fb5a2f38f3de3b5b6
Hanif Mustofa <hanifindramayou@gmail.com>
2026-06-10 02:25:31 +0700

    Add OrderController show method (get order detail)

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -26,7 +26,7 @@ class OrderController extends Controller
 
         return response()->json([
             'success' => true, 
-            'order_code' => $orderCode
+            'order_code' => $orderCode 
         ]);
     }
 
